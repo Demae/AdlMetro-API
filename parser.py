a2991e52e1f8c5f11480187c9c091a1fd2790e26
Demae <56660883+Demae@users.noreply.github.com>
2024-02-21 14:01:51 +1030

    Add files via upload

--- parser.py
+++ parser.py
@@ -5,13 +5,15 @@ import json
 df = pd.read_csv('table.csv', header=None)
 
 # Define the custom field names
-field_names = ['Bus No.', 'Rego No.', 'Chassis', 'VIN', 'Body', 'Body No.', 'Body Date', 'Seating', 'Livery', 'Depot']  # Adjust as needed
+field_names = ['id', 'registration', 'chassis', 'vin', 'body', 'body_no', 'body_date', 'seating', 'livery', 'depot']  # Adjust as needed
 
 # Rename columns with custom field names
 df.columns = field_names
 
 # Set the first column as the index
-df.set_index('Bus No.', inplace=True)  # Assuming the first column is the core item
+df.set_index('id', inplace=True)  # Assuming the first column is the core item
+
+df['operator'] = "Torrens Transit"
 
 # Convert the DataFrame to a dictionary
 data_dict = df.to_dict(orient='index')
